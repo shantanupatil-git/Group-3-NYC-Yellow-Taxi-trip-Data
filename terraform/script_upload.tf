5222768909a04b55c83ceafaba0bbcc04a5e066f
Gyandeep42 <yadavgyan2002@email.com>
2025-08-06 01:16:49 +0530

    automate ci/cd pipeline first try-2

--- terraform/script_upload.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_object" "etl_script" {
-  bucket = aws_s3_bucket.source_bucket.id
-  key    = var.script_s3_key
-  source = "../etl-glue-script.py"
-  etag   = filemd5("../etl-glue-script.py")
-}
