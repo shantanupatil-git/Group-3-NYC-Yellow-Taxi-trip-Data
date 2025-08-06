4b32f71181707aaa19ed72d1895e68d27c5fc0de
Gyandeep42 <yadavgyan2002@email.com>
2025-08-06 11:06:49 +0530

    automation testing-1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "source_bucket" {
-  value = aws_s3_bucket.source_bucket.bucket
+  value = var.raw_bucket_name
 }
 
 output "target_bucket" {
-  value = aws_s3_bucket.target_bucket.bucket
+  value = var.cleaned_bucket_name
 }
 
 output "glue_database_name" {
-  value = aws_glue_catalog_database.nyc_db.name
+  value = aws_glue_catalog_database.this.name
 }
 
 output "glue_job_name" {
-  value = aws_glue_job.etl_job.name
+  value = aws_glue_job.this.name
 }
 
 output "crawler_name" {
-  value = aws_glue_crawler.nyc_crawler.name
+  value = aws_glue_crawler.this.name
 }
