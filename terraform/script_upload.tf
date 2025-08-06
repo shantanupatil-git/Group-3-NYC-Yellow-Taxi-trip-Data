daffa1fab974a5b40d50225637b977c26fe3b813
Gyandeep42 <yadavgyan2002@email.com>
2025-08-06 11:27:45 +0530

    automation testing-1

--- terraform/script_upload.tf
+++ terraform/script_upload.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "etl_script" {
   bucket = var.raw_bucket_name
   key    = var.etl_script_s3_key
-  source = "${path.module}/../etl-glue-script.py"
-  etag   = filemd5("${path.module}/../etl-glue-script.py")
+  source = "${path.module}/../etl/etl-glue-script.py"
+  etag   = filemd5("${path.module}/../etl/etl-glue-script.py")
 }
